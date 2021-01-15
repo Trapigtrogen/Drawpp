37009913cfb82eb975402a07ed57fd2f3d6e3bc1
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-01-15 13:41:09 +0200

    glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <KHR/khrplatform.h>
+#include <GLFW/glfw3.h>
 
 int main() {
     std::cout << "Hello World!";
+    glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE);
     return 0;
 }
