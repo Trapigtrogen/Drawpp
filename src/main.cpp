e444df1ee88d0e15c6386647e3ec7cdf325f1567
Leevi Riitakangas <leevi.riitakangas@gmail.com>
2021-01-15 13:44:44 +0200

    window test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <KHR/khrplatform.h>
+#include <stdio.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 int main() {
-    std::cout << "Hello World!";
-    glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE);
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glfwSwapInterval(1);
+
+    char c = getchar();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
