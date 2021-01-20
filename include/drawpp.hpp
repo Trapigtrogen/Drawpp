372469126c311c148f46d5a714e5eb8fb137d473
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-01-20 15:37:34 +0200

    shapes api

--- include/drawpp.hpp
+++ include/drawpp.hpp
@@ -2,3 +2,22 @@
 void draw();
 void setup();
 void runApp();
+
+// Shapes 2D
+void arc(int x, int y, int width, int height, int start, int stop);
+void arc(int x, int y, int width, int height, int start, int stop, int mode);
+void circle(int x, int y, int radius);
+void ellipse(int x, int y, int width, int height);
+void line(int x1, int y1, int x2, int y2);
+void point(int x, int y);
+void quad(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4);
+void rect(int x, int y, int w, int h);
+void square(int x, int y, int size);
+void triangle(int x1, int y1, int x2, int y2, int x3, int y3);
+
+// Shapes 3D
+void point(int x, int y, int z);
+void line(int x1, int y1, int z1, int x2, int y2, int z2);
+void box(int size);
+void box(int width, int height, int dimension);
+void sphere(int radius);
--- tests/shape.cpp
+++ tests/shape.cpp
@@ -13,3 +13,29 @@ int main() {
 	runApp();
 	return 0;
 }
+
+void rect(float x, float y, float w, float h) {
+	float vertices[] = {
+		-w/2 + x, h/2 + y, // Vertex 1 (X, Y)
+		-w/2 + x, -h/2 + y, // Vertex 2 (X, Y)
+		w/2 + x, -h/2 + y,  // Vertex 3 (X, Y)
+		-w/2 + x, h/2 + y, // Vertex 1 (X, Y)
+		w/2 + x, -h/2 + y,  // Vertex 3 (X, Y)
+		w/2 + x, h/2 + y  // Vertex 3 (X, Y)
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo); // Generate 1 buffer
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+	float vertices[] = {
+		x1, y1, // Vertex 1 (X, Y)
+		x2, y2, // Vertex 2 (X, Y)
+		x3, y3  // Vertex 3 (X, Y)
+	};
+}
