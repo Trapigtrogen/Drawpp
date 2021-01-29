113bb99ef5fdfabb322331f5cd73562cdc42e108
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-01-29 11:38:48 +0200

    temp code cleanup

--- src/drawpp.cpp
+++ src/drawpp.cpp
@@ -46,56 +46,6 @@ void background(DImage image)
 
 void rect(int x, int y, int width, int height)
 {
-    Shader shader;
-    shader = loadShadersFromFile("D:/Projects/CPP/vertexShader.shader", "D:/Projects/CPP/fragmentShader.shader");
-
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    //float* vertices = Primitive::triangle(x - width / 2, y - height / 2, 0, x - width / 2, y + height / 2, 0, x + width / 2, y + height / 2, 0);
-    //float* vertices = Primitive::rectangle(x-width/2,y-height/2,0, x-width/2,y+height/2,0, x+width/2,y+height/2,0, x+width/2,y-height/2,0);
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
-    };
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
-    // render loop
-    // -----------
-    while(1) {
-        // render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // draw our first triangle
-        glUseProgram(shader.getId());
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-    }
-
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shader.getId());
 
 }
 
