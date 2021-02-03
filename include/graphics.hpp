b703befb7513bffa02d05d8c2c61388b3af40a9e
Leevi Riitakangas <leevi.riitakangas@gmail.com>
2021-02-03 15:51:01 +0200

    added graphics properties, and color and transform functions

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -1,14 +1,46 @@
 #ifndef DPP_GRAPHICS_H
 #define DPP_GRAPHICS_H
 
+#include <vector3.hpp>
+#include <matrix4.hpp>
+
+class Image;
+
+enum ColorMode
+{
+    RGB,
+    HSB
+};
+
+struct GraphicsProperties
+{
+    //Color last_background_color;
+    ColorMode colormode;
+    float color_max1 = 255;
+    float color_max2 = 255;
+    float color_max3 = 255;
+    float color_maxa = 255;
+    bool use_stroke = false;
+    //Color stroke_color;
+    //int stroke_weight;
+    bool use_tint = false;
+    //Color tint_color;
+    bool use_fill = false;
+    //Color fill_color;
+
+};
+
 class DGraphics
 {
     friend class Application;
 public:
 
+    ///\brief Create a DGraphics object with a \p width by \p height frame
     DGraphics(int width, int height);
 
     ///\brief Set this DGraphics object to be the current render target
+    ///
+    ///Always call beginDraw when you change the target DGraphics object. Not calling it will result in undefined behaviour.
     void beginDraw();
 
     ///\brief Explicitly end drawing to this buffer
@@ -16,11 +48,74 @@ public:
     ///This does not need to be called if you call beginDraw() on another DGraphics object.
     void endDraw();
 
+    //void fill(Color rgba);
+    //void fill(Color rgb, float alpha);
+    //void fill(float grey);
+    //void fill(float grey, float alpha);
+
+    //affected by colorMode
+    //void fill(float v1, float v2, float v3);
+    //void fill(float v1, float v2, float v3, float alpha);
+    	
+    //void background(Color rgba)
+    //void background(Color rgb, float alpha)
+    //void background(float gray)
+    //void background(float gray, float alpha)
+    //void background(const Image* image)
+    //void background(const Image& image)
+
+    //affected by colorMode
+    //void background(float v1, float v2, float v3)
+    //void background(float v1, float v2, float v3, float alpha)
+
+    //void clear();
+
+    void colorMode(ColorMode mode);
+    void colorMode(ColorMode mode, float max);
+    void colorMode(ColorMode mode, float max1, float max2, float max3);
+    void colorMode(ColorMode mode, float max1, float max2, float max3, float maxA);
+
+
+    //void tint(Color rgba);
+    //void tint(Color rgb, float alpha);
+    //void tint(float grey);
+    //void tint(float grey, float alpha);
+
+    //affected by colorMode
+    //void tint(float v1, float v2, float v3);
+    //void tint(float v1, float v2, float v3, float alpha);
+
+    void noFill();
+
+    void noStroke();
+
+    void noTint();
+
+    void translate(float x, float y);
+    void translate(float x, float y, float z);
+    void translate(const DVector& t);
+
+    void rotate(float angle);
+
+    void rotateX(float angle);
+    void rotateY(float angle);
+    void rotateZ(float angle);
+
+    	
+    void scale(float s);
+    void scale(float x, float y);
+    void scale(float x, float y, float z);
+    void scale(const DVector& s);
+
 private:
 
+    ///\brief Get the texture id for this graphics object
     unsigned int get_texture_id();
 
     void set_current();
+
+    GraphicsProperties properties;
+    DMatrix4 transform_mat = DMatrix4::identity();
 
     unsigned int buffer_id = -1;
     unsigned int texture_id = -1;
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -2,6 +2,27 @@
 #include <glad/glad.h>
 #include <debug.hpp>
 
+const float primitive_square[] = 
+{
+    -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     1.0f,  1.0f, 0.0f,
+    -1.0f, -1.0f, 0.0f,
+     1.0f,  1.0f, 0.0f,
+    -1.0f,  1.0f, 0.0f,
+};
+
+float coords_quad[] = 
+{
+    -1.0f, -1.0f,
+     1.0f, -1.0f, 
+     1.0f,  1.0f, 
+    -1.0f, -1.0f, 
+     1.0f,  1.0f, 
+    -1.0f,  1.0f, 
+};
+
+
 DGraphics::DGraphics(int width, int height)
 {
     buffer_width = static_cast<unsigned int>(width);
@@ -23,6 +44,7 @@ DGraphics::DGraphics(int width, int height)
 
 void DGraphics::beginDraw()
 {
+    properties = GraphicsProperties();
     glBindFramebuffer(GL_FRAMEBUFFER,buffer_id);
     glViewport(0,0,buffer_width,buffer_height);
 }
@@ -31,6 +53,108 @@ void DGraphics::endDraw()
 {
     glBindFramebuffer(GL_FRAMEBUFFER,0);
 }
+
+void DGraphics::colorMode(ColorMode mode)
+{
+    properties.colormode = mode;
+}
+
+void DGraphics::colorMode(ColorMode mode, float max)
+{
+    properties.colormode = mode;
+    properties.color_max1 = max;
+    properties.color_max2 = max;
+    properties.color_max3 = max;
+    properties.color_maxa = max;
+}
+
+void DGraphics::colorMode(ColorMode mode, float max1, float max2, float max3)
+{
+    properties.colormode = mode;
+    properties.color_max1 = max1;
+    properties.color_max2 = max2;
+    properties.color_max3 = max3;
+}
+
+void DGraphics::colorMode(ColorMode mode, float max1, float max2, float max3, float maxA)
+{
+    properties.colormode = mode;
+    properties.color_max1 = max1;
+    properties.color_max2 = max2;
+    properties.color_max3 = max3;
+    properties.color_maxa = maxA;
+}
+
+void DGraphics::noFill()
+{
+    properties.use_fill = false;
+}
+
+void DGraphics::noStroke()
+{
+    properties.use_stroke = false;
+}
+
+void DGraphics::noTint()
+{
+    properties.use_tint = false;
+}
+
+void DGraphics::translate(float x, float y)
+{
+    translate(x,y,0);
+}
+
+void DGraphics::translate(float x, float y, float z)
+{
+    translate({x,y,z});
+}
+
+void DGraphics::translate(const DVector& t)
+{
+    transform_mat.translate(t);
+}
+
+void DGraphics::rotate(float angle)
+{
+    rotateZ(angle);
+}
+
+void DGraphics::rotateX(float angle)
+{
+    transform_mat.rotate<Axis::X>(angle);
+}
+
+void DGraphics::rotateY(float angle)
+{
+    transform_mat.rotate<Axis::Y>(angle);
+}
+
+void DGraphics::rotateZ(float angle)
+{
+    transform_mat.rotate<Axis::Z>(angle);
+}
+
+void DGraphics::scale(float s)
+{
+    scale({s,s,s});
+}
+
+void DGraphics::scale(float x, float y)
+{
+    scale({x,y,1});
+}
+
+void DGraphics::scale(float x, float y, float z)
+{
+    scale({x,y,z});
+}
+
+void DGraphics::scale(const DVector& s)
+{
+    transform_mat.scale(s);
+}
+
 
 unsigned int DGraphics::get_texture_id()
 {
