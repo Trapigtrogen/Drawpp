deaaf81bab96992ea544383d0655522ee67fc7a4
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-02-04 15:56:52 +0200

    flip

--- src/image.cpp
+++ src/image.cpp
@@ -105,6 +105,8 @@ DImage DImage::loadImage(const std::string& fileName)
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
+	stbi_set_flip_vertically_on_load(1);
+
 	return DImage(pixels, m_texture);
 
 }
