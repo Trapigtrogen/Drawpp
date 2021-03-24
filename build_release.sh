7d0bbc884f8e9824edae23e18d3160d12581575d
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-03-24 14:23:46 +0200

    Linux script changes

--- build_release.sh
+++ build_release.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 cd `dirname $(realpath $0)`
 
+# Either create or empty release folder
 if [ ! -d ./release ]; then
   mkdir -p ./release;
+else
+    rm -rf ./release/*
 fi
 
 while true
@@ -70,11 +73,24 @@ rm ./Makefile
 
 echo "Copying headers over..."
 # copy include files
-cp -r ../include/ ./
-#cp -r ../external/glad/include/ ./include/
-#cp -r ../external/glfw/include/GLFW ./include/
-#cp -r ../external/stb ./include/
-#cp -r ../external/nanosvg ./include/
+mkdir include
+
+cp ../include/application.hpp ./include/
+cp ../include/color.hpp ./include/
+cp ../include/debug.hpp ./include/
+cp ../include/drawpp.hpp ./include/
+cp ../include/graphics.hpp ./include/
+cp ../include/image.hpp ./include/
+cp ../include/input.hpp ./include/
+cp ../include/keys.hpp ./include/
+cp ../include/matrix4.hpp ./include/
+cp ../include/noise.hpp ./include/
+cp ../include/random.hpp ./include/
+cp ../include/shader.hpp ./include/
+cp ../include/shape.hpp ./include/
+cp ../include/time.hpp ./include/
+cp ../include/vector3.hpp ./include/
+cp ../include/font.hpp ./include/
 
 echo "Combining libraries..."
 # combine libraries
@@ -82,7 +98,9 @@ mkdir lib1 && cd lib1 && ar -x ../lib/libOpenGLGraphicsLibrary.a
 cd ..
 mkdir lib2 && cd lib2 && ar -x ../external/glfw/src/libglfw3.a
 cd ..
-ar -qc libOpenGLGraphicsLibrary.a ./lib1/*.o ./lib2/*.o
+mkdir lib3 && cd lib3 && ar -x ../external/freetype/libfreetype.a
+cd ..
+ar -qc libOpenGLGraphicsLibrary.a ./lib1/*.o ./lib2/*.o ./lib3/*.o
 
 # move tests to main folder
 if [ -d ./bin/tests ] && [ $debugBuild = "ON" ]; then
@@ -102,16 +120,18 @@ echo "cmake_minimum_required(VERSION 3.13.4)">./examples/CMakeLists.txt
 echo "set(CMAKE_CXX_STANDARD 11)">>./examples/CMakeLists.txt
 echo "set(CMAKE_CXX_STANDARD_REQUIRED ON)">>./examples/CMakeLists.txt
 echo "project(GraphicsLib_Examples)">>./examples/CMakeLists.txt
-echo "link_libraries(\${CMAKE_SOURCE_DIR}/../OpenGLGraphicsLibrary.lib)">>./examples/CMakeLists.txt
+echo "link_libraries(\${CMAKE_SOURCE_DIR}/../libOpenGLGraphicsLibrary.a)">>./examples/CMakeLists.txt
 
 echo "include_directories(\${CMAKE_SOURCE_DIR}/../include)">>./examples/CMakeLists.txt
 echo "set(EXECUTABLE_OUTPUT_PATH \${CMAKE_BINARY_DIR}/bin)">>./examples/CMakeLists.txt
-#add example files here like this
-#echo add_executable(example_window window.cpp)>>./examples/CMakeLists.txt
 
-#for now, no examples
-echo "message(SEND_ERROR \"No examples!\")">>./examples/CMakeLists.txt
-
+# Add examples to CMakeLists.txt
+for file in ./examples/*.cpp
+do
+    filename=$(basename "$file")
+    fname="${filename%.*}"
+    echo "add_executable(example_$fname $filename)">>./examples/CMakeLists.txt
+done
 
 
 # FINAL CLEANUP:
@@ -122,6 +142,7 @@ rm -rf ./bin
 # Remove temp folders
 rm -rf ./lib1
 rm -rf ./lib2
+rm -rf ./lib3
 
 # Remove library folders
 rm -rf ./lib
