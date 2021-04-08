59cb21b4e11a30b23bfb997921d7df1fc8294300
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-04-08 14:04:23 +0300

    Turned the perlin noise test to an example

--- examples/example_perlin-noise.cpp
+++ examples/example_perlin-noise.cpp
@@ -7,52 +7,65 @@ int octaves = 8;
 int seed = 1;
 float nscale = 1.3f;
 
+bool updateNoise = true;
+
 void setup()
 {
     setTitle("Perlin noise");
     noStroke();
+
+    textFont(loadFont("assets/Tuffy.ttf", 30));
 }
 
 void draw(float)
 {
-    printf("Octaves: %i, Seed: %i, Scale: %f\n", octaves, seed, nscale);
-
-    background(0, 0, 0);
-    for(int x = 0; x < width; x++)
+    // Limit redrawing the whole noise until something has changed
+    if (updateNoise) 
     {
-        for(int y = 0; y < height; y++)    
+        background(0, 0, 0);
+
+        for (int x = 0; x < width; x++)
         {
-            fill((noise(x, y) * 255));
-            //rect(x*5, y*5, 5, 5); // pixel like
-            circle(x*5, y*5, 5); // Smoother           
+            for (int y = 0; y < height; y++)
+            {
+                fill((noise(x, y) * 255));
+                circle(x * 5, y * 5, 5);       
+            }
         }
+
+        fill(255, 255, 255);
+        text("Seed: " + std::to_string(seed) + "\nOctaves: " + std::to_string(octaves) + "\nScale: " + std::to_string(nscale), 30, 30);
+        text("Controls:\nR - Random seed\nE - Previous seed\nT - Next seed\nW - Octaves up\nS - Octaves down\nD - Scale up\nA - Scale down", 30, 150);
+
+        updateNoise = false;
     }
+
 }
 
 void keyPressed()
 {
-    if(key == VK_I)
+    if(key == VK_W)
     {
         octaves++;
         noiseDetails(octaves);
         noiseSeed(seed);
     }
 
-    else if(key == VK_K)
+    else if(key == VK_S)
     {
         octaves--;
         noiseDetails(octaves);
         noiseSeed(seed);
     }
 
-    else if(key == VK_W)   
+    else if(key == VK_D)   
     {
         nscale += 0.1f;
         noiseScale(nscale);
         noiseSeed(seed);
     }
 
-    else if(key == VK_S)     
+    else if(key == VK_A)     
     {
         nscale -= 0.1f;
         noiseScale(nscale);
@@ -60,13 +73,13 @@ void keyPressed()
     }
 
 
-    else if(key == VK_L)   
+    else if(key == VK_T)   
     {
         seed++;
         noiseSeed(seed);
     }
 
-    else if(key == VK_J)   
+    else if(key == VK_E)   
     {
         seed--;
         noiseSeed(seed);
@@ -77,6 +90,8 @@ void keyPressed()
         seed = randomInt(100000);
         noiseSeed(seed);
     }
+
+    updateNoise = true;
 }
 
 int main()
