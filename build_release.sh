3769528c82e6d9e8ad391e7b3acaecc68db3daff
Samuli Piipponen <piipponen.samuli@gmail.com>
2021-04-21 15:02:42 +0300

    linus release fixed

--- build_release.sh
+++ build_release.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+# Start in the folder where the script is
 cd `dirname $(realpath $0)`
+
 # Either create or empty release folder
 if [ ! -d ./release ]; then
     mkdir -p ./release;
@@ -90,6 +93,7 @@ cp ../include/shape.hpp ./include/
 cp ../include/vector3.hpp ./include/
 cp ../include/font.hpp ./include/
 
+
 echo "Combining libraries..."
 # Create temp script
 echo "create libDrawpp.a">libCombiner.mri
@@ -103,11 +107,15 @@ ar -M <libCombiner.mri
 # Remove script
 rm libCombiner.mri
 
+
+# TESTS:
 # move tests to main folder
 if [ -d ./bin/tests ] && [ $debugBuild = "ON" ]; then
     echo "Copying tests over..."
+    rm -r ./tests
     mv ./bin/tests ./
 fi
+
 
 # EXAMPLES:
 if [ ! -d ./examples ]; then
@@ -115,16 +123,77 @@ if [ ! -d ./examples ]; then
 fi
 
 cp -r ../examples ./
+mkdir ./examples/build
+mkdir ./examples/build/bin
+mv ./examples/assets ./examples/build/bin/
 
-# Create CMakeLists.txt file for examples
+# Create CMakeLists.txt file for building examples
 echo "cmake_minimum_required(VERSION 3.13.4)">./examples/CMakeLists.txt
+echo "include(CheckIncludeFiles)">>./examples/CMakeLists.txt
+echo "include(CheckFunctionExists)">>./examples/CMakeLists.txt
 echo "set(CMAKE_CXX_STANDARD 11)">>./examples/CMakeLists.txt
 echo "set(CMAKE_CXX_STANDARD_REQUIRED ON)">>./examples/CMakeLists.txt
 echo "project(GraphicsLib_Examples)">>./examples/CMakeLists.txt
+echo "set(THREADS_PREFER_PTHREAD_FLAG ON)">>./examples/CMakeLists.txt
+echo "find_package(Threads REQUIRED)">>./examples/CMakeLists.txt
+echo "set(DPP_WLIBS \"\")">>./examples/CMakeLists.txt
+echo "set(DPP_WINCS \"\")">>./examples/CMakeLists.txt
+echo "include(CMakeDependentOption)">>./examples/CMakeLists.txt
+echo "cmake_dependent_option(DPP_USE_WINDOW_WAYLAND \"Use Wayland as window system\" OFF \"UNIX;NOT APPLE\" OFF)">>./examples/CMakeLists.txt
+echo "if (DPP_USE_WINDOW_WAYLAND)">>./examples/CMakeLists.txt
+echo "    find_package(ECM REQUIRED NO_MODULE)">>./examples/CMakeLists.txt
+echo "    list(APPEND CMAKE_MODULE_PATH \"\${ECM_MODULE_PATH}\")">>./examples/CMakeLists.txt
+echo "    find_package(Wayland REQUIRED Client Cursor Egl)">>./examples/CMakeLists.txt
+echo "    find_package(WaylandScanner REQUIRED)">>./examples/CMakeLists.txt
+echo "    find_package(WaylandProtocols 1.15 REQUIRED)">>./examples/CMakeLists.txt
+echo "    list(APPEND DPP_WINCS \"\${Wayland_INCLUDE_DIRS}\")">>./examples/CMakeLists.txt
+echo "    list(APPEND DPP_WLIBS \"\${Wayland_LIBRARIES}\" \"\${CMAKE_THREAD_LIBS_INIT}\")">>./examples/CMakeLists.txt
+echo "    find_package(XKBCommon REQUIRED)">>./examples/CMakeLists.txt
+echo "    list(APPEND DPP_WINCS \"\${XKBCOMMON_INCLUDE_DIRS}\")">>./examples/CMakeLists.txt
+echo "    check_include_files(xkbcommon/xkbcommon-compose.h HAVE_XKBCOMMON_COMPOSE_H)">>./examples/CMakeLists.txt
+echo "    check_function_exists(memfd_create HAVE_MEMFD_CREATE)">>./examples/CMakeLists.txt
+echo "    if (NOT (\"\${CMAKE_SYSTEM_NAME}\" STREQUAL \"Linux\"))">>./examples/CMakeLists.txt
+echo "        find_package(EpollShim)">>./examples/CMakeLists.txt
+echo "        if (EPOLLSHIM_FOUND)">>./examples/CMakeLists.txt
+echo "            list(APPEND DPP_WINCS \"\${EPOLLSHIM_INCLUDE_DIRS}\")">>./examples/CMakeLists.txt
+echo "            list(APPEND DPP_WLIBS \"\${EPOLLSHIM_LIBRARIES}\")">>./examples/CMakeLists.txt
+echo "        endif()">>./examples/CMakeLists.txt
+echo "    endif()">>./examples/CMakeLists.txt
+echo "elseif (UNIX)">>./examples/CMakeLists.txt
+echo "    find_package(X11 REQUIRED)">>./examples/CMakeLists.txt
+echo "    list(APPEND DPP_WINCS \"\${X11_X11_INCLUDE_PATH}\")">>./examples/CMakeLists.txt
+echo "    list(APPEND DPP_WLIBS \"\${X11_X11_LIB}\"" >>./examples/CMakeLists.txt
+echo "                            \"\${CMAKE_THREAD_LIBS_INIT}\")">>./examples/CMakeLists.txt
+echo "    if (NOT X11_Xrandr_INCLUDE_PATH)">>./examples/CMakeLists.txt
+echo "        message(FATAL_ERROR \"RandR headers not found; requires libxrandr development package\")">>./examples/CMakeLists.txt
+echo "    endif()">>./examples/CMakeLists.txt
+echo "    if (NOT X11_Xinerama_INCLUDE_PATH)">>./examples/CMakeLists.txt
+echo "        message(FATAL_ERROR \"Xinerama headers not found; requires libxinerama development package\")">>./examples/CMakeLists.txt
+echo "    endif()">>./examples/CMakeLists.txt
+echo "    if (NOT X11_Xkb_INCLUDE_PATH)">>./examples/CMakeLists.txt
+echo "        message(FATAL_ERROR \"XKB headers not found; requires X11 development package\")">>./examples/CMakeLists.txt
+echo "    endif()">>./examples/CMakeLists.txt
+echo "    if (NOT X11_Xcursor_INCLUDE_PATH)">>./examples/CMakeLists.txt
+echo "        message(FATAL_ERROR \"Xcursor headers not found; requires libxcursor development package\")">>./examples/CMakeLists.txt
+echo "    endif()">>./examples/CMakeLists.txt
+echo "    if (NOT X11_Xi_INCLUDE_PATH)">>./examples/CMakeLists.txt
+echo "        message(FATAL_ERROR \"XInput headers not found; requires libxi development package\")">>./examples/CMakeLists.txt
+echo "    endif()">>./examples/CMakeLists.txt
+echo "    list(APPEND DPP_WINCS \"\${X11_Xrandr_INCLUDE_PATH}\"">>./examples/CMakeLists.txt
+echo "                            \"\${X11_Xinerama_INCLUDE_PATH}\"">>./examples/CMakeLists.txt
+echo "                            \"\${X11_Xkb_INCLUDE_PATH}\"">>./examples/CMakeLists.txt
+echo "                            \"\${X11_Xcursor_INCLUDE_PATH}\"">>./examples/CMakeLists.txt
+echo "                            \"\${X11_Xi_INCLUDE_PATH}\")">>./examples/CMakeLists.txt
+echo "else()">>./examples/CMakeLists.txt
+echo "    message(FATAL_ERROR \"No supported window system was found\")">>./examples/CMakeLists.txt
+echo "endif()">>./examples/CMakeLists.txt
+echo "link_libraries(\${CMAKE_DL_LIBS})">>./examples/CMakeLists.txt
 echo "link_libraries(\${CMAKE_SOURCE_DIR}/../libDrawpp.a)">>./examples/CMakeLists.txt
-
+echo "link_libraries(\${DPP_WLIBS})">>./examples/CMakeLists.txt
 echo "include_directories(\${CMAKE_SOURCE_DIR}/../include)">>./examples/CMakeLists.txt
 echo "set(EXECUTABLE_OUTPUT_PATH \${CMAKE_BINARY_DIR}/bin)">>./examples/CMakeLists.txt
+echo "include_directories(\${DPP_WINCS})">>./examples/CMakeLists.txt
+
 
 # Add examples to CMakeLists.txt
 for file in ./examples/*.cpp
@@ -136,16 +205,10 @@ done
 
 
 # FINAL CLEANUP:
-
 echo "final cleanup..."
-#rm -rf ./bin
-
-# Remove library folders
-#rm -rf ./lib
-#rm -rf ./external
-
-# remove other unnecessary folders
-#rm -rf ./CMakeFiles
-#rm -rf ./tests
+rm -rf ./bin
+rm -rf ./lib
+rm -rf ./external
+rm -rf ./CMakeFiles
 
 echo "Done!"
